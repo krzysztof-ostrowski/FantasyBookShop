68100b1c40b0d9a7386e02761127e394f984f5cf
Krzysztof Ostrowski <ostrowski021@gmail.com>
2023-12-07 23:18:29 +0100

    Deleting from cart added

--- FantasyBookShop/Client/Services/CartService/CartService.cs
+++ FantasyBookShop/Client/Services/CartService/CartService.cs
@@ -1,13 +1,18 @@
 ﻿using Blazored.LocalStorage;
+using FantasyBookShop.Shared.Dtos;
+using System.Net.Http.Json;
 
 namespace FantasyBookShop.Client.Services.CartService
 {
     public class CartService : ICartService
     {
         private readonly ILocalStorageService _localStorage;
-        public CartService(ILocalStorageService localStorage)
+        private readonly HttpClient _http;
+
+        public CartService(ILocalStorageService localStorage, HttpClient http)
         {
             _localStorage = localStorage;
+            _http = http;
         }
 
         public event Action OnChange;
@@ -22,11 +27,35 @@ namespace FantasyBookShop.Client.Services.CartService
             OnChange.Invoke();
         }
 
+        public async Task<List<CartBookResponseDto>> GetCartBooks()
+        {
+            var cartItems = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            var response = await _http.PostAsJsonAsync("api/cart/books", cartItems);
+            var cartBooks = await response.Content.ReadFromJsonAsync<ServiceResponse<List<CartBookResponseDto>>>();
+            return cartBooks.Data;
+        }
+
         public async Task<List<CartItem>> GetCartItems()
         {
             var cart = await GetCart();
 
             return cart;
+        }
+
+        public async Task RemoveBookFromCart(int productId, int productTypeId)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            if(cart == null)
+            {
+                return;
+            }
+            var cartItem = cart.Find(b=>b.BookId == productId && b.BookTypeId == productTypeId);
+            if (cartItem != null)
+            {
+                cart.Remove(cartItem);
+                await _localStorage.SetItemAsync("cart", cart);
+                OnChange.Invoke();
+            }
         }
 
         private async Task<List<CartItem>> GetCart()
--- FantasyBookShop/Client/Services/CartService/ICartService.cs
+++ FantasyBookShop/Client/Services/CartService/ICartService.cs
@@ -1,9 +1,13 @@
-﻿namespace FantasyBookShop.Client.Services.CartService
+﻿using FantasyBookShop.Shared.Dtos;
+
+namespace FantasyBookShop.Client.Services.CartService
 {
     public interface ICartService
     {
         event Action OnChange;
         Task AddToCart(CartItem cartItem);
         Task<List<CartItem>> GetCartItems();
+        Task<List<CartBookResponseDto>> GetCartBooks();
+        Task RemoveBookFromCart(int productId, int productTypeId);
     }
 }
